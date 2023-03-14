8d95f34daefbbb8d3bc9a078815a7a0a7cc4a35f
DrewMax <andrew.reviresco@gmail.com>
2023-03-14 15:15:35 +0100

    basic structure

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// querySelectors to HTML
+
+// request and response API
+// GET && POST for API
+
+// API and key all linked up
+// weather API and sports API
+// json response 
+// local storage for sign up sheet
+// interactive map
+
+function test() {
+    console.log("this is a test")
+}
+
+test();
