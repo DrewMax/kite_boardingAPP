b885658c2b33d99a78ee6d3319c8466dfebcfab6
DrewMax <andrew.reviresco@gmail.com>
2023-03-22 09:15:14 +0100

    search by month

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,79 @@
-// querySelectors to HTML
-const searchForm = document.querySelector(".searchForm");
-const submitBtn = document.querySelector(".submitBtn");
+const fetchData = () => {
+    const getRequest = new XMLHttpRequest();
+  
+    getRequest.open("GET", "database", true);
+  
+    getRequest.onload = function() {
+      if (this.status === 200) {
+        const data = JSON.parse(this.responseText);
+        // need to figure out where to put all this data
+        console.log(data);
+      }
+    };
+  
+    getRequest.onerror = function() {
+      console.error("That didnt work!");
+    };
+  
+    getRequest.send();
+  }
 
-var tasks = [];
 
-var saveBtn = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-};
+function captureFormData() {
+    const form = document.getElementById("searchForm");
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault(); 
+  
+      // get form data
+      const data = {
+       temperature: form.elements.temperature.value,
+        location: form.elements.location.value,
+        experience: form.elements.experience.value,
+        pressure: form.elements.pressure.value,
+        outdoor: form.elements.outdoor.value
+      };
+  
+      let response = "";
+      // need condtions for these if statement
+      // need to create combinations that respond with different locations
+      let location = getLocation(${location});
 
-// request and response API
-// GET && POST for API
-
-// API and key all linked up
-// weather API and sports API
-// json response 
-// local storage for sign up sheet
-// interactive map
-// animated news reel
 
 
-function test() {
-    console.log("this is a test")
-}
+    if (data.temperature === "hot" && data.location === ) {
+      fetchData();
 
-test();
 
-document.addEventListener("click", submitBtn())
+    } if (data.location === "Cape Town" || data.location === "phuket") {
+      fetchData();
+    } if (data.experience === "Novice" || data.experience === "") {
+      fetchData();
+    }
+
+    // display response
+    console.log(response)
+    });
+  }
+  
+  function handleFormSubmit() {
+    // retrieve form data from localStorage
+    const jsonData = localStorage.getItem("formData");
+  
+    // convert JSON data to JavaScript object
+    const data = JSON.parse(jsonData);
+  
+    // do something with the form data
+    console.log(data);
+  
+    // clear form data from localStorage
+    localStorage.removeItem("formData");
+  }
+  
+  const submitButton = document.getElementById("submitBtn");
+  submitButton.addEventListener("click", handleFormSubmit);
+  
+  
+  
+  
+  
