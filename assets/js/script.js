699aa04b8dd8c3637af3b19e55524214ea2d04e2
DrewMax <andrew.reviresco@gmail.com>
2023-03-24 09:10:16 +0100

    work

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,210 +4,6 @@ function monthEl() {
   
 }
 
-// Get a reference to the button element
-const myButton1 = document.getElementById("button1");
-
-// Add an event listener to the button
-myButton1.addEventListener("click", (event) => {
-event.preventDefault();
-  // Loop through the season array and apply it to Google Maps
-  for (let i = 0; i < seasonArr_1.length; i++) {
-    const data = seasonArr_1[i];
-    // Add a marker to the map at the given position
-    const marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: data.title
-    });
-  }
-});
-
-const myButton2 = document.getElementById("button2");
-
-// Add an event listener to the button
-myButton2.addEventListener("click", (event) => {
-event.preventDefault();
-  // Loop through the season array and apply it to Google Maps
-  for (let i = 0; i < seasonArr_2.length; i++) {
-    const data = seasonArr_2[i];
-    // Add a marker to the map at the given position
-    const marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: data.title
-    });
-  }
-});
-
-const myButton3 = document.getElementById("button3");
-
-// Add an event listener to the button
-myButton3.addEventListener("click", (event) => {
-event.preventDefault();
-  // Loop through the season array and apply it to Google Maps
-  for (let i = 0; i < seasonArr_3.length; i++) {
-    const data = seasonArr_3[i];
-    // Add a marker to the map at the given position
-    const marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: data.title
-    });
-  }
-});
-
-const myButton4 = document.getElementById("button4");
-
-// Add an event listener to the button
-myButton4.addEventListener("click", (event) => {
-event.preventDefault();
-  // Loop through the season array and apply it to Google Maps
-  for (let i = 0; i < seasonArr_4.length; i++) {
-    const data = seasonArr_4[i];
-    // Add a marker to the map at the given position
-    const marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: data.title
-    });
-  }
-});
-
-const myButton5 = document.getElementById("button5");
-
-// Add an event listener to the button
-myButton5.addEventListener("click", (event) => {
-event.preventDefault();
-  // Loop through the season array and apply it to Google Maps
-  for (let i = 0; i < seasonArr_5.length; i++) {
-    const data = seasonArr_5[i];
-    // Add a marker to the map at the given position
-    const marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: data.title
-    });
-  }
-});
-
-const myButton6 = document.getElementById("button6");
-
-// Add an event listener to the button
-myButton6.addEventListener("click", (event) => {
-event.preventDefault();
-  // Loop through the season array and apply it to Google Maps
-  for (let i = 0; i < seasonArr_6.length; i++) {
-    const data = seasonArr_6[i];
-    // Add a marker to the map at the given position
-    const marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: data.title
-    });
-  }
-});
-
-const myButton7 = document.getElementById("button7");
-
-// Add an event listener to the button
-myButton7.addEventListener("click", (event) => {
-event.preventDefault();
-  // Loop through the season array and apply it to Google Maps
-  for (let i = 0; i < seasonArr_7.length; i++) {
-    const data = seasonArr_7[i];
-    // Add a marker to the map at the given position
-    const marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: data.title
-    });
-  }
-});
-
-const myButton8 = document.getElementById("button8");
-
-// Add an event listener to the button
-myButton8.addEventListener("click", (event) => {
-event.preventDefault();
-  // Loop through the season array and apply it to Google Maps
-  for (let i = 0; i < seasonArr_8.length; i++) {
-    const data = seasonArr_8[i];
-    // Add a marker to the map at the given position
-    const marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: data.title
-    });
-  }
-});
-
-const myButton9 = document.getElementById("button9");
-
-// Add an event listener to the button
-myButton9.addEventListener("click", (event) => {
-event.preventDefault();
-  // Loop through the season array and apply it to Google Maps
-  for (let i = 0; i < seasonArr_9.length; i++) {
-    const data = seasonArr_9[i];
-    // Add a marker to the map at the given position
-    const marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: data.title
-    });
-  }
-});
-
-const myButton10 = document.getElementById("button10");
-
-// Add an event listener to the button
-myButton10.addEventListener("click", (event) => {
-event.preventDefault();
-  // Loop through the season array and apply it to Google Maps
-  for (let i = 0; i < seasonArr_10.length; i++) {
-    const data = seasonArr_10[i];
-    // Add a marker to the map at the given position
-    const marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: data.title
-    });
-  }
-});
-
-const myButton11 = document.getElementById("button11");
-
-// Add an event listener to the button
-myButton11.addEventListener("click", (event) => {
-event.preventDefault();
-  // Loop through the season array and apply it to Google Maps
-  for (let i = 0; i < seasonArr_11.length; i++) {
-    const data = seasonArr_11[i];
-    // Add a marker to the map at the given position
-    const marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: data.title
-    });
-  }
-});
-
-const myButton12 = document.getElementById("button12");
-
-// Add an event listener to the button
-myButton12.addEventListener("click", (event) => {
-event.preventDefault();
-  // Loop through the season array and apply it to Google Maps
-  for (let i = 0; i < seasonArr_12.length; i++) {
-    const data = seasonArr_12[i];
-    // Add a marker to the map at the given position
-    const marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: data.title
-    });
-  }
-});
 
 // Get the modal and close button
 const modal = document.getElementById("signup-modal");
@@ -232,6 +28,51 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+let strings = [];
+const input = document.getElement('input');
+const submit = document.getElementById('submit');
+
+// add a click event listener to the button
+submit.addEventListener('click', () => {
+  // get the current array of strings from local storage, or create a new one if it doesn't exist
+  const strings = JSON.parse(localStorage.getItem('strings')) || [];
+
+  // add the new string to the array
+  strings.push(input.value);
+
+  // save the updated array back to local storage
+  localStorage.setItem('strings', JSON.stringify(strings));
+
+  // clear the input field
+  input.value = '';
+});
+
+
+let carousel = document.querySelector(".carousel");
+let prevBtn = document.querySelector(".prev");
+let nextBtn = document.querySelector(".next");
+let images = document.querySelectorAll(".carousel img");
+let currentIndex = 0;
+
+prevBtn.addEventListener("click", function() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  updateCarousel();
+});
+
+nextBtn.addEventListener("click", function() {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateCarousel();
+});
+
+function updateCarousel() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.left = `${(i - currentIndex) * 100}%`;
+  }
+}
+
+
+
+
 // Arrays for the map
 var seasonArr_1 = [["Watamu Beach","-3.3546866","40.0170135",120,"kitesurf-watamu-beach-kenya"],["Mui Ne","10.9341368","108.2864947",142,"kitesurf-mui-ne-vietnam"],["Jericoacoara & Prea","-2.8123580","-40.4203163",136,"kitesurf-jericoacoara-prea-brazil"],["Jambiani","-6.3165949","39.5458169",129,"kitesurf-jambiani-zanzibar"],["Fuerteventura, Playa Sotavento","28.1307545","-14.2445413",119,"kitesurf-fuerteventura-playa-sotavento-canary-islands"],["Blougbergstrand, Cape Town","-33.8241693","18.4770965",146,"kitesurf-blougbergstrand-cape-town-south-africa"],["Dakhla","23.9200652","-15.7648696",124,"kitesurf-dakhla-morocco"],["Safety Bay, Perth","-32.3054511","115.7129860",137,"kitesurf-safety-bay-perth-australia"],["Paracas","-13.8580624","-76.2534428",126,"kitesurf-paracas-peru"],["La Ventana - Baja California Sur","24.0462212","-109.9872173",122,"kitesurf-la-ventana-baja-california-sur-mexico"],["Sal, Santa Maria","16.6142328","-22.8972836",149,"kitesurf-sal-santa-maria-cabo-verde"],["Christchurch Estuary","-43.5493254","172.7045802",147,"kitesurf-christchurch-estuary-new-zealand"],["Punta Chame","8.6521188","-79.7033000",138,"kitesurf-punta-chame-panama"],["Paje","-6.2662258","39.5356086",131,"kitesurf-paje-zanzibar"],["Boracay  Island","11.9657799","121.9285980",143,"kitesurf-boracay-island-philippines"],["Providenciales - Long Bay Beach","21.7721084","-72.1634656",128,"kitesurf-providenciales-long-bay-beach-turks-and-caicos"],["Kalpitiya","8.2265547","79.7349542",141,"kitesurf-kalpitiya-sri-lanka"]];
 
@@ -270,7 +111,7 @@ function initMap() {
     let currentInfoWindow;
     // create a function that will loop through the array and create markers for all locations
     // create markers for all locations
-    for (var i=0; i<${locationArr}.length; i++) {
+    for (var i=0; i<seasonArr_1.length; i++) {
         const location = seasonArr_1[i];
         const marker = new google.maps.Marker({
             position: {lat: parseFloat(location[1]), lng: parseFloat(location[2])},
